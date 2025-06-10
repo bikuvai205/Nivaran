506084f710527a2417bb38888ac479fbb87e8f7e
bikuvai205 <itsmeking205@gmail.com>
2025-06-10 07:39:56 +0545

    env created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     hello world
+      <h1>Nivaran</h1>
+      <p>Welcome to Nivaran, your personal AI assistant.</p>
+      <p>Click the button below to start a conversation.</p>
+      <button onClick={() => alert('Button clicked!')}>Start Conversation</button>
+    </>
+  )
+}
+
+export default App
